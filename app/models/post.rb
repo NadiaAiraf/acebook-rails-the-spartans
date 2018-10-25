871a91969d323ecf1b31c368d97b53c53df3cbeb
NadiaAiraf <aidanfaria@gmail.com>
2018-10-25 13:53:26 +0100

    trying to get usernames to input into db

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  def self.find_post_user(user)
-    user_id = Post.find(user).user_id
+  def self.find_post_user(post_id)
+    user_id = Post.find(post_id).user_id
     return User.find(user_id).email
   end
 end
