695c9bfc51274a6e9d94a2a580b2294132d8b22e
NadiaAiraf <aidanfaria@gmail.com>
2018-10-25 14:50:03 +0100

    committing tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
+  scenario "can sign in" do
+    visit "/users/sign_up"
+    fill_in 'user_email', with: 'aidan@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    puts 'hello'
+    click_button 'Sign up'
+    visit '/posts'
+    expect(page).to have_content('This is where you create a new post New post')
+  end
+
   scenario "Can submit posts and view them" do
+    visit "/users/sign_up"
+    fill_in 'user_email', with: 'aidan@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    puts 'hello'
+    click_button 'Sign up'
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
