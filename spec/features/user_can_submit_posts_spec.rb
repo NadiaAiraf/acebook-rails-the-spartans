b690a002dd6c87d97719424f72577d88f6f69c47
Antonio <antoniodaria@msn.com>
2018-10-26 12:40:22 +0100

    Refactored feature test with web helpers

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,63 +1,31 @@
 require 'rails_helper'
 require 'timecop'
+require 'web_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "User can sign_up" do
-    visit "/users/sign_up"
-    fill_in "user_email", with: "testuser@yahoo.com"
-    fill_in "user_password", with: "testpassword1234"
-    fill_in "user_password_confirmation", with: "testpassword1234"
-    click_button "Sign up"
-    visit "/posts"
+    sign_up
     expect(page).to have_content("This is where you create a new post")
   end
 
   scenario "Can submit posts and view them" do
-    visit "/users/sign_up"
-    fill_in "user_email", with: "testuser@yahoo.com"
-    fill_in "user_password", with: "testpassword1234"
-    fill_in "user_password_confirmation", with: "testpassword1234"
-    click_button "Sign up"
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    sign_up
+    new_post
+    expect(page).to have_content("Hello, People!")
   end
 
   scenario "Can see time when the post was made" do
-    visit "/users/sign_up"
-    fill_in "user_email", with: "testuser@yahoo.com"
-    fill_in "user_password", with: "testpassword1234"
-    fill_in "user_password_confirmation", with: "testpassword1234"
-    click_button "Sign up"
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, People!"
-    click_button "Submit"
-    post_time = Time.now
-    Timecop.freeze(post_time) do
-    expect(page).to have_content(post_time.strftime("%a %b %e %T"))
-  end
+    sign_up
+    new_post
+    set_times {expect(page).to have_content(post_time.strftime("%a %b %e %T"))}
 end
 
   scenario "Can check that the newest post appears first" do
-    visit "/users/sign_up"
-    fill_in "user_email", with: "testuser@yahoo.com"
-    fill_in "user_password", with: "testpassword1234"
-    fill_in "user_password_confirmation", with: "testpassword1234"
-    click_button "Sign up"
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, People!"
-    click_button "Submit"
+    sign_up
+    new_post
     click_link "New post"
     fill_in "Message", with: "Hello, Spartans!"
     click_button "Submit"
-    post_time = Time.now
-    Timecop.freeze(post_time) do
-    expect(page).to have_content("Hello, Spartans! link " + post_time.strftime("%a %b %e %T")+ " " + "testuser@yahoo.com Hello, People!")
+    set_times {expect(page).to have_content("Hello, Spartans! link " + post_time.strftime("%a %b %e %T")+ " " + "testuser@yahoo.com Hello, People!")}
   end
-end
-
 end
