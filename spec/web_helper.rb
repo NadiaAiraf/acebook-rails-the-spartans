0a009c8dd3a814aaec5709a9696b2c23428c24f5
Antonio <antoniodaria@msn.com>
2018-10-26 16:48:03 +0100

    removing the username field from web helper

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,6 @@
 def sign_up
   visit "/users/sign_up"
   fill_in "user_email", with: "testuser@yahoo.com"
-  fill_in "user_username", with: "testuser"
   fill_in "user_password", with: "testpassword1234"
   fill_in "user_password_confirmation", with: "testpassword1234"
   click_button "Sign up"
