4dff62067fdfb6eb100e769cf1ff8328d7a1816a
NicGiles <n.r.giles1@gmail.com>
2018-10-29 12:41:30 +0000

    Updated posts model with display date method. Updated views to include new method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,15 @@ class Post < ApplicationRecord
   def user_username
     user.username
   end
+
+  def date_shown
+    if updated_at == nil
+      t = created_at.in_time_zone("London")
+      t.strftime("%a %b %e %T")
+    else
+      t = updated_at.in_time_zone("London")
+      t.strftime("%a %b %e %T")
+    end
+  end
+
 end
