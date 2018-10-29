e6440d47dce2357152a9b49f1e9939c92f2af6fa
Antonio <antoniodaria@msn.com>
2018-10-29 12:21:26 +0000

    some rubocop changes

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'timecop'
 require 'web_helper'
 
-RSpec.feature "Delete posts", type: :feature do
-  scenario "User can delete posts" do
+RSpec.feature 'Delete posts', type: :feature do
+  scenario 'User can delete posts' do
     sign_up
     new_post
-    find("#delete1").click
-    expect(page).not_to have_content("Hello, People!")
+    find('#delete1').click
+    expect(page).not_to have_content('Hello, People!')
   end
 
   scenario "User can't delete other people's posts" do
     sign_up
     new_post
-    click_link "Logout"
+    click_link 'Logout'
     sign_up_2
-    expect{ find('#delete1') }.to raise_error
+    expect { find('#delete1') }.to raise_error
   end
 
-  scenario "upon deletion, user stays on the homepage" do
+  scenario 'upon deletion, user stays on the homepage' do
     sign_up
     new_post
-    find("#delete1").click
-    expect(page).to have_content "This is where you create a new post"
+    find('#delete1').click
+    expect(page).to have_content 'This is where you create a new post'
   end
 end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'timecop'
 require 'web_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "User can sign_up" do
+RSpec.feature 'Timeline', type: :feature do
+  scenario 'User can sign_up' do
     sign_up
-    expect(page).to have_content("This is where you create a new post")
+    expect(page).to have_content('This is where you create a new post')
   end
 
-  scenario "Can submit posts and view them" do
+  scenario 'Can submit posts and view them' do
     sign_up
     new_post
-    expect(page).to have_content("Hello, People!")
+    expect(page).to have_content('Hello, People!')
   end
 
-  scenario "Can see time when the post was made" do
+  scenario 'Can see time when the post was made' do
     sign_up
     new_post
-    set_times {expect(page).to have_content(post_time.strftime("%a %b %e %T"))}
- end
+    set_times { expect(page).to have_content(post_time.strftime('%a %b %e %T')) }
+  end
 
-  scenario "Can check that the newest post appears first" do
-   sign_up
-   new_post
-   click_link "New post"
-   fill_in "Message", with: "Hello, Spartans!"
-   click_button "Submit"
-   set_times {expect(page).to have_content("Hello, Spartans! link " + post_time.strftime("%a %b %e %T")+ " " + "Hello, People!")}
- end
+  scenario 'Can check that the newest post appears first' do
+    sign_up
+    new_post
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, Spartans!'
+    click_button 'Submit'
+    set_times { expect(page).to have_content('Hello, Spartans! link ' + post_time.strftime('%a %b %e %T') + ' ' + 'Hello, People!') }
+  end
 end
