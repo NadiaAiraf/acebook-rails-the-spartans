70b3accdd0708f589ef21eca0393d7af27633060
NicGiles <n.r.giles1@gmail.com>
2018-10-29 12:34:57 +0000

    Updated feature test to encorporate displayed times updating alongside edited posts

--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -25,4 +25,19 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Hello, People!")
     expect(page).not_to have_link('Edit Post')
   end
+
+  scenario 'Edited posts display latest time' do
+    sign_up
+    Timecop.freeze(Time.zone.parse('12:00 13 October 2018'))
+    new_post
+    expect(page).to have_content("Hello, People!")
+    expect(page).to have_content('Sat Oct 13 12:00:00')
+    Timecop.freeze(Time.zone.parse('12:45 13 October 2018'))
+    click_on('Edit Post')
+    fill_in('Message', with: 'lots of changes to post')
+    click_button('Update!')
+    expect(page).not_to have_content('Hello, People!')
+    expect(page).not_to have_content('Sat Oct 13 12:00:00')
+    expect(page).to have_content('Sat Oct 13 12:45:00')
+  end
 end
