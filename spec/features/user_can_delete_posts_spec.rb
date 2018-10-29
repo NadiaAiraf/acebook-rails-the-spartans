af491a697117304061dbc889f0ce608766315988
Antonio <antoniodaria@msn.com>
2018-10-29 11:38:56 +0000

    adding feature test for delte action and link to delete in index.html

---
+++ spec/features/user_can_delete_posts_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'timecop'
+require 'web_helper'
+
+RSpec.feature "Delete posts", type: :feature do
+  scenario "User can delete posts" do
+    sign_up
+    new_post
+    find("#delete1").click
+    expect(page).not_to have_content("Hello, People!")
+  end
+
+
+
+
+
+ #  scenario "Can submit posts and view them" do
+ #    sign_up
+ #    new_post
+ #
+ #  end
+ #
+ #  scenario "Can see time when the post was made" do
+ #    sign_up
+ #    new_post
+ #    set_times {expect(page).to have_content(post_time.strftime("%a %b %e %T"))}
+ # end
+ #
+ #  scenario "Can check that the newest post appears first" do
+ #   sign_up
+ #   new_post
+ #   click_link "New post"
+ #   fill_in "Message", with: "Hello, Spartans!"
+ #   click_button "Submit"
+ #   set_times {expect(page).to have_content("Hello, Spartans! link " + post_time.strftime("%a %b %e %T")+ " " + "Hello, People!")}
+ # end
+end
