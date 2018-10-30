f7618444416b74a9f586bbd3f60f32c6ab082931
NicGiles <n.r.giles1@gmail.com>
2018-10-30 16:46:00 +0000

    Updated views to display likes correctly,and  with a capital L.

--- spec/features/user_likes_spec.rb
+++ spec/features/user_likes_spec.rb
@@ -7,30 +7,37 @@ RSpec.feature 'Like posts', type: :feature do
     sign_up
     new_post
     like
-    expect(page).to have_content('likes: 1')
+    expect(page).to have_content('1 Likes')
   end
 
   scenario 'Multiple users can like a post' do
     sign_up
     new_post
     like
-    expect(page).to have_content('likes: 1')
+    expect(page).to have_content('1 Likes')
     logout
     sign_up_2
     like
-    expect(page).not_to have_content('likes: 1')
-    expect(page).to have_content('likes: 2')
+    expect(page).not_to have_content('1 Likes')
+    expect(page).to have_content('2 Likes')
   end
 
   scenario "User can unlike something they previously thought was quite good but now they don't think is very good" do
     sign_up
     new_post
     like
-    expect(page).to have_content('likes: 1')
+    expect(page).to have_content('1 Likes')
     unlike
-    expect(page).to have_content('likes: 0')
+    expect(page).to have_content('0 Likes')
   end
 
-
+  scenario "Like button switches to unlike button when user likes something cool" do
+    sign_up
+    new_post
+    like
+    expect(page).to have_content('Unlike')
+    unlike
+    expect(page).to have_content('Like')
+  end
 
 end
