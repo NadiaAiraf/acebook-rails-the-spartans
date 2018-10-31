7924b2c01e5387400f3c64ab9088e03cc2529a93
Florence Hapgood <flohapgood@icloud.com>
2018-10-31 14:27:50 +0000

    Link changed to have a capital, solving the problem of merging conflicts

--- spec/features/user_can_add_comments_spec.rb
+++ spec/features/user_can_add_comments_spec.rb
@@ -6,10 +6,8 @@ RSpec.feature 'User can Comment on posts', type: :feature do
   scenario 'User can post a comment' do
     sign_up
     new_post
-    click_link "link"
+    click_link "Link"
     fill_in 'comment[body]', with: "Pig"
-    #find('#hello').set('pig')
-
     click_button"Create Comment"
     expect(page).to have_content('Pig')
   end
