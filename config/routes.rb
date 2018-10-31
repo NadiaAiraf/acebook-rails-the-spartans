2f075c7e0c739882cb4b19cbd8ea4a46895eed25
nrg <37775158+NicGiles@users.noreply.github.com>
2018-10-31 14:15:22 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   resources :posts, :users do
-  resources :comments
+    resources :comments
+  end
   post 'post/:id/likes', to: 'posts#like', as: :likes
   post 'post/:id/unlikes', to: 'posts#unlike', as: :unlikes
-  resources :posts, :users
 end
