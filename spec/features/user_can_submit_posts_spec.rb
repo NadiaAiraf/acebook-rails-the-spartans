8d0d4313ba83bc2e5471a8178bd587c03aff307b
NicGiles <n.r.giles1@gmail.com>
2018-11-02 10:15:08 +0000

    Updated web helpers to reflect new CSS changes. All RSpec tests now passing

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature 'Timeline', type: :feature do
   scenario 'User can sign_up' do
     sign_up
-    expect(page).to have_content('This is where you create a new post')
+    expect(page).to have_content("Your Board Create a New Post")
   end
 
   scenario 'Can submit posts and view them' do
@@ -19,9 +19,7 @@ RSpec.feature 'Timeline', type: :feature do
   scenario 'Can check that the newest post appears first' do
     sign_up
     new_post
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, Spartans!'
-    click_button 'Submit'
+    new_post_2
     set_times { expect(page).to have_content('Hello, Spartans! link ' + post_time.strftime('%a %b %e %T') + ' ' + 'Hello, People!') }
   end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,5 +2,5 @@
 
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-end
+# RSpec.describe Post, type: :model do
+# end
